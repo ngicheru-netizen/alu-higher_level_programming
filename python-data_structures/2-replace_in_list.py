3ca80d6d456a82e0a38f51830c346971870d71b6
ngicheru-netizen <n.gicheru@alustudent.com>
2026-03-06 01:44:46 +0200

    2.4

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,12 +5,5 @@ def replace_in_list(my_list, idx, element):
         return None
     if idx >= len(my_list):
         return None
-
-
-my_list = [1, 2, 3]
-idx = 1
-new_element = 4
-result = replace_in_list(my_list, idx, new_element)
-replace_in_list(my_list, idx, new_element)
-print(result)
-print(my_list)
+    my_list[idx] = element
+    return my_list
