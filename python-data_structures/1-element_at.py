0941f9a7e40f987b2d418f19d5e4d4f29d44ce60
ngicheru-netizen <n.gicheru@alustudent.com>
2026-03-06 01:17:11 +0200

    added elements to code

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,5 +3,9 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > my_list:
+    elif idx >= len(my_list):
         return None
+    return my_list[idx]
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
