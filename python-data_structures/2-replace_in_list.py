6fd96a3f6357a7fa3d07e41cc46f8ec774b5c96d
ngicheru-netizen <n.gicheru@alustudent.com>
2026-03-06 01:50:00 +0200

    Added variables

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -7,3 +7,11 @@ def replace_in_list(my_list, idx, element):
         return None
     my_list[idx] = element
     return my_list
+
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 4
+new_list = replace_in_list(my_list, idx, new_element)
+print(new_list)
+print(my_list)
