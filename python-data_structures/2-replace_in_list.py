4a15ca677e2fc45ba54519901d3bf55c49750324
ngicheru-netizen <n.gicheru@alustudent.com>
2026-03-06 06:34:57 +0200

    my_list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,7 +4,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0:
         return None
     if idx >= len(my_list):
-        return None
+        return my_list
     my_list[idx] = element
-    return my_list
-
+    return my_list[idx]
