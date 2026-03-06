1a51949d71d32e5cfd7c25b3dabf121df241c293
ngicheru-netizen <n.gicheru@alustudent.com>
2026-03-06 06:31:32 +0200

    changes

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -8,10 +8,3 @@ def replace_in_list(my_list, idx, element):
     my_list[idx] = element
     return my_list
 
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 4
-new_list = replace_in_list(my_list, idx, new_element)
-print(new_list)
-print(my_list)
