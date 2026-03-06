4f168e356f68a7f69576597726a6be1a40aebfbc
ngicheru-netizen <n.gicheru@alustudent.com>
2026-03-06 07:31:54 +0200

    {}

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    my_list = [1, 2, 3, 4, 5, 6]
+    list = my_list
     for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+        print("{}".format(my_list[i]))
 
-
-print_reversed_list_integer()
--- python-data_structures/test.py
+++ python-data_structures/test.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-replace_in_list = __import__('2-replace_in_list').replace_in_list
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-list = [1, 2, 3]
-idx = 1
-new_element = 4
-new_list = replace_in_list(list, idx, new_element)
-
-print(new_list)
-print(list)
+list = [1, 2, "H", 9]
+print_reversed_list_integer(list)
