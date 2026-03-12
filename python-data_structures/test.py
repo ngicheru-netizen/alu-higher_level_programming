bd46466bb7f1ff80f289a5dfa576b7438be813bf
ngicheru-netizen <n.gicheru@alustudent.com>
2026-03-12 13:58:07 +0200

    6

--- python-data_structures/test.py
+++ python-data_structures/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-delete_at = __import__("11-delete_at").delete_at
+square_matrix_simple = __import__("0-square_matrix_simple").square_matrix_simple
 
-my_list = [1, 2, 3, 4, 5]
-idx = 2
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
