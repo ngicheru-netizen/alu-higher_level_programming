8a5c3a23e5b98730414ebea3e9867e1dfc410f95
ngicheru-netizen <n.gicheru@alustudent.com>
2026-06-14 15:27:47 +0300

    2-10

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fun = "C is fun";
-const cool = "Python is cool";
-const amazing = "JavaScript is amazing";
+const fun = 'C is fun';
+const cool = 'Python is cool';
+const amazing = 'JavaScript is amazing';
 
 console.log(fun);
 console.log(cool);
---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const input = process.argv[2];
+const num = Number(input);
+
+console.log(num);
