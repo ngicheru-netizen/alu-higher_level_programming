8ad861d5829a4929d07a5b3f1e085f2b01f710b5
ngicheru-netizen <n.gicheru@alustudent.com>
2026-06-14 15:41:48 +0300

    5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-const input = process.argv[2];
-const num = Number(input);
+const num = parseInt(process.argv[2]);
 
-console.log(num);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
+}
