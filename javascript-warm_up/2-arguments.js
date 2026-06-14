a92af4e6b02b637b52c8756032affa7727bd0dd0
ngicheru-netizen <n.gicheru@alustudent.com>
2026-06-14 15:39:08 +0300

    Changes in 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const arg = process.argv[2];
+const count = process.argv.length - 2;
 
-if (arg === undefined) {
+if (count === 0) {
   console.log('No argument');
+} else if (count === 1) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
